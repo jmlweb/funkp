652a2c46d4b5d2b591512d0cb4741b63085ecd1e
José Manuel Lucas <josemanuel@jmlweb.es>
2018-06-03 22:26:00 +0200

    Coverage improvements

--- src/curryProps/index.spec.js
+++ src/curryProps/index.spec.js
@@ -9,6 +9,21 @@ describe('curryProps', () => {
     const expected = true;
     assert.equal(tested, expected, 'Should be a instance of function');
   });
+  it('returns a function 2', () => {
+    const tested = curriedPropsOperation({ a: 8 }) instanceof Function;
+    const expected = true;
+    assert.equal(tested, expected, 'Should be a instance of function');
+  });
+  it('works without passing arity', () => {
+    const tested = curryProps(Object.keys)({ a: 8 });
+    const expected = ['a'];
+    assert.deepStrictEqual(tested, expected, 'Should return [a]');
+  });
+  it('works without passing value', () => {
+    const tested = curryProps(Object.keys)().length;
+    const expected = 0;
+    assert.equal(tested, expected, 'Should return 0');
+  });
   it('works', () => {
     const tested = curriedPropsOperation({ a: 8 })({ b: 2 })({ c: 3 });
     const expected = 2;
